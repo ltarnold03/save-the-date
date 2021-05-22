10a262ec9b320e0c26a525364c103b3491f3eb51
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-05-22 17:08:23 -0500

    add timeblocks

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+//var currentDay = moment().format("2010-10-20 4:30", "YYYY-MM-DD HH:mm"); )
+
+
+
+
+
+
+
+// var currentDay = moment().year(year).month(month).date(day)
+
+
+// var getTime = moment().set('hour', 8);
+// var 
+
+// moment().get('date');
+// moment().get(unit) === moment()[unit](1);
+// moment().set('hour', 8);
