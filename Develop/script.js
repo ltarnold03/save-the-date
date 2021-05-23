71a5a5adea4bc6f82a0f318ded63439fcaf40380
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-05-22 19:57:06 -0500

    add current date

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,5 @@
-//var currentDay = moment().format("2010-10-20 4:30", "YYYY-MM-DD HH:mm"); )
-
-
-
-
-
-
-
-// var currentDay = moment().year(year).month(month).date(day)
-
-
-// var getTime = moment().set('hour', 8);
-// var 
-
-// moment().get('date');
-// moment().get(unit) === moment()[unit](1);
-// moment().set('hour', 8);
+var currentDay = document.querySelector("#currentDay");
+var currentDay = moment().format("dddd MMMM Do HH:mm");
+var currentDayInput = new Date();
+$("#currentDay").append(currentDay);
+console.log(currentDay);
